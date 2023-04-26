ef9092532faf22ce5b346d8ec0d4ac89991f6f75
Yu7in <94485@naver.com>
2023-04-27 00:54:17 +0900

    section_7: Use annotation-based configuration to dependency injection

--- src/main/java/moviebuddy/domain/MovieFinder.java
+++ src/main/java/moviebuddy/domain/MovieFinder.java
@@ -4,11 +4,16 @@ import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MovieFinder {
 
 	private final MovieReader movieReader;
 	
-	public MovieFinder(MovieReader movieReader) {
+	public MovieFinder(@Qualifier("csvMovieReader") MovieReader movieReader) {
 		this.movieReader = Objects.requireNonNull(movieReader);
 	}
 
