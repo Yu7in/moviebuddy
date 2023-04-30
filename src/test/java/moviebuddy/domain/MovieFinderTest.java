0ba6fe51c852370c7bc4a6f91bd62557f22e06cf
Yu7in <94485@naver.com>
2023-04-30 21:50:58 +0900

    section_8: Refactor testing with test-context framework

--- src/test/java/moviebuddy/domain/MovieFinderTest.java
+++ src/test/java/moviebuddy/domain/MovieFinderTest.java
@@ -4,8 +4,13 @@ import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import moviebuddy.MovieBuddyFactory;
 import moviebuddy.domain.Movie;
@@ -14,10 +19,22 @@ import moviebuddy.domain.MovieFinder;
 /**
  * @author springrunner.kr@gmail.com
  */
+@SpringJUnitConfig(MovieBuddyFactory.class)
+//@ExtendWith(SpringExtension.class)
+//@ContextConfiguration(classes = MovieBuddyFactory.class)
 public class MovieFinderTest {
 	
-	final static ApplicationContext applicationContext = new AnnotationConfigApplicationContext(MovieBuddyFactory.class);
-	final static MovieFinder movieFinder = applicationContext.getBean(MovieFinder.class);
+	@Autowired MovieFinder movieFinder;
+	
+//	@Autowired
+//	MovieFinderTest(MovieFinder movieFinder) {
+//		this.movieFinder = movieFinder;
+//	}
+	
+//	@Autowired
+//	void setMovidFinder(MovieFinder movieFinder) {
+//		this.movieFinder = movieFinder;
+//	}
 	
 	@Test
 	void NotEmpty_directedBy() {
@@ -31,7 +48,7 @@ public class MovieFinderTest {
 		Assertions.assertEquals(225, movie.size());
 	}
 
-	public static void main(String[] args) {
+	public void main(String[] args) {
 		List<Movie> result = movieFinder.directedBy("Michael Bay");
 		assertEquals(3, result.size());
 
