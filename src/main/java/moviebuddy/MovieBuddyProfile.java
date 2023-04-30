4d8699f017100b78b343d8ab0756e58c21967626
Yu7in <94485@naver.com>
2023-05-01 01:21:35 +0900

    section_10: Refactor metadata read strategy by profiles

---
+++ src/main/java/moviebuddy/MovieBuddyProfile.java
@@ -0,0 +1,11 @@
+package moviebuddy;
+
+public class MovieBuddyProfile {
+
+	public static final String CSV_MODE = "csv_mode";
+	public static final String XML_MODE = "xml_mode";
+	
+	// 상수를 모아 둘 클래스로 생성자를 private으로 숨김 (생성자 차단)
+	private MovieBuddyProfile() {}
+	
+}
--- src/main/java/moviebuddy/domain/MovieFinder.java
+++ src/main/java/moviebuddy/domain/MovieFinder.java
@@ -13,7 +13,7 @@ public class MovieFinder {
 
 	private final MovieReader movieReader;
 	
-	public MovieFinder(@Qualifier("csvMovieReader") MovieReader movieReader) {
+	public MovieFinder(MovieReader movieReader) {
 		this.movieReader = Objects.requireNonNull(movieReader);
 	}
 
