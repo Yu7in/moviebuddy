f832c393f0d4a412fe088101c52094605023770f
Yu7in <94485@naver.com>
2023-05-17 23:52:36 +0900

    section_20: Use modularized cache abstraction for caching metadata

--- src/main/java/moviebuddy/MovieBuddyFactory.java
+++ src/main/java/moviebuddy/MovieBuddyFactory.java
@@ -2,34 +2,35 @@ package moviebuddy;
 
 import java.util.concurrent.TimeUnit;
 
-import javax.cache.annotation.CacheResult;
-
-import org.aopalliance.aop.Advice;
-import org.springframework.aop.Advisor;
-import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
-import org.springframework.aop.support.DefaultPointcutAdvisor;
-import org.springframework.aop.support.annotation.AnnotationMatchingPointcut;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurer;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.cache.interceptor.CacheErrorHandler;
+import org.springframework.cache.interceptor.CacheResolver;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.cache.interceptor.SimpleCacheErrorHandler;
+import org.springframework.cache.interceptor.SimpleCacheResolver;
+import org.springframework.cache.interceptor.SimpleKeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
-
-import moviebuddy.cache.CachingAdvice;
-import moviebuddy.cache.CachingAspect;
 
 @Configuration
 @PropertySource("/application.properties")
 @ComponentScan(basePackages = { "moviebuddy" })
 @Import({ MovieBuddyFactory.DomainModuleConfig.class, MovieBuddyFactory.DataSourceModuleConfig.class })
-@EnableAspectJAutoProxy
-public class MovieBuddyFactory {
+@EnableCaching
+public class MovieBuddyFactory implements CachingConfigurer {
 	
 	@Bean
 	public Jaxb2Marshaller jaxb2Marshaller() {
@@ -61,11 +62,34 @@ public class MovieBuddyFactory {
 		// Advisor = PointCut(대상 선정 알고리즘) + Advice(부가기능)
 		return new DefaultPointcutAdvisor(pointcut, advice);
 	}
-	*/
 	
 	@Bean
 	public CachingAspect cachingAspect(CacheManager cacheManager) {
 		return new CachingAspect(cacheManager);
+	}
+	*/
+	
+	@Override
+	public CacheManager cacheManager() {
+		return caffeineCacheManager();
+	}
+
+	@Override
+	public CacheResolver cacheResolver() {
+		return new SimpleCacheResolver(caffeineCacheManager());
+	}
+
+	@Override
+	public KeyGenerator keyGenerator() {
+		return new SimpleKeyGenerator();
+	}
+
+	@Override
+	public CacheErrorHandler errorHandler() {
+//		@EnableAsync -> AsyncConfigurer
+//		@EnableScheduling -> SchedulingConfigurer
+		
+		return new SimpleCacheErrorHandler();
 	}
 	
 	@Configuration
